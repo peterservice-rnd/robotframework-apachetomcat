1a91f4be61e92da65dac2e4a5cbf8a480fa86925
Aleskarova, Dinara <dinara.aleskarova@simbirsoft.com>
2018-01-23 16:43:07 +0400

    Added setup.py and setup.cfg files

---
+++ setup.py
@@ -0,0 +1,31 @@
+"""Setup module for Robot Framework Apache Tomcat Manager Library package."""
+
+from codecs import open
+from os import path
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='robotframework-apachetomcat',
+    version='1.0.0',
+    description='A Robot Framework Apache Tomcat Manager Library',
+    long_description=long_description,
+    url='https://github.com/peterservice-rnd/robotframework-apachetomcat',
+    license='Apache License, Version 2.0',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='testing robotframework apache tomcat',
+    py_modules=['ApacheTomcatManager'],
+    install_requires=['requests', 'robotframework'],
+)
